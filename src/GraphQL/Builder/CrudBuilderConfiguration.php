15bf013d15ade23626c6aa94945e64ae31e11357
Damien Belingheri <belingheridamien@gmail.com>
2022-01-20 19:52:19 +0100

    treeNode config

--- src/GraphQL/Builder/CrudBuilderConfiguration.php
+++ src/GraphQL/Builder/CrudBuilderConfiguration.php
@@ -4,38 +4,28 @@ declare(strict_types=1);
 
 namespace Sparklink\GraphQLToolsBundle\GraphQL\Builder;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class CrudBuilderConfiguration implements ConfigurationInterface
 {
+    public const GET            = 'get';
+    public const LIST           = 'list';
+    public const CREATE         = 'create';
+    public const UPDATE         = 'update';
+    public const DELETE         = 'delete';
+    public const ALL            = 'all';
+    public const ALL_OPERATIONS = [self::ALL, self::GET, self::LIST, self::CREATE, self::UPDATE, self::DELETE];
+
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('builder');
         $rootNode    = $treeBuilder->getRootNode();
         $rootNode
             ->children()
-                ->scalarNode('access')->defaultNull()->end()
-                ->scalarNode('permission')->defaultNull()->end()
-                ->arrayNode('names')
-                    ->children()
-                        ->scalarNode('list')->defaultValue('<type>List')->end()
-                        ->scalarNode('delete')->defaultValue('Delete<Type>')->end()
-                    ->end()
-                ->end()
-
+                ->append($this->addOperationsConfig())
                 ->arrayNode('types')->useAttributeAsKey('types')->prototype('array')
-                    ->validate()
-                            ->always(function ($v) {
-                                // check if the tree as children key 'operations'.
-
-                                if (null !== $v['access'] && null !== $v['permission']) {
-                                    throw new \InvalidArgumentException('Cannot use both "access" and "permission" keys in configuration for type "operations".');
-                                }
-
-                                return $v;
-                            })
-                    ->end()
                     ->children()
                         ->arrayNode('operations')
                             ->beforeNormalization()->ifString()->castToArray()->end()
@@ -47,7 +37,7 @@ class CrudBuilderConfiguration implements ConfigurationInterface
                                         if ('all' === $value) {
                                             return $v;
                                         }
-                                        if (!\in_array($value, ['all', 'get', 'list', 'create', 'update', 'delete'])) {
+                                        if (!\in_array($value, self::ALL_OPERATIONS)) {
                                             throw new \InvalidArgumentException(sprintf('Invalid value "%s" for "operations" key in configuration for type "operations".', $value));
                                         }
 
@@ -56,23 +46,114 @@ class CrudBuilderConfiguration implements ConfigurationInterface
                                 })
                             ->end()
                         ->end()
-                        ->scalarNode('access')->defaultNull()->end()
-                        ->scalarNode('permission')->defaultNull()->end()
-                        ->arrayNode('list')
-                            ->children()
-                                ->scalarNode('permission')->defaultNull()->end()
-                                ->arrayNode('orderBy')
-                                    ->useAttributeAsKey('name')
-                                        ->prototype('scalar')->end()
-                                    ->end()
-                                ->end()
-                            ->end()
-                        ->end()
+                        ->append($this->addOperationGetConfig())
+                        ->append($this->addOperationListConfig())
+                        ->append($this->addOperationCreateConfig())
+                        ->append($this->addOperationUpdateConfig())
+                        ->append($this->addOperationDeleteConfig())
                     ->end()
                 ->end()
             ->end()
         ;
 
         return $treeBuilder;
+    }
+
+    protected function addOperationsConfig()
+    {
+        $treeBuilder = new TreeBuilder('default');
+        $rootNode    = $treeBuilder->getRootNode();
+
+        $rootNode
+            ->children()
+                ->append($this->addOperationGetConfig())
+                ->append($this->addOperationListConfig())
+                ->append($this->addOperationCreateConfig())
+                ->append($this->addOperationUpdateConfig())
+                ->append($this->addOperationDeleteConfig())
+            ->end();
+
+        return $rootNode;
+    }
+
+    public function addAccessConfig(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->validate()
+                ->always(function ($v) {
+                    if (null !== $v['access'] && null !== $v['permission']) {
+                        throw new \InvalidArgumentException('Cannot use both "access" and "permission" keys in configuration for type "operations".');
+                    }
+
+                    return $v;
+                })
+            ->end()
+            ->children()
+                ->scalarNode('permission')->defaultNull()->end()
+                ->scalarNode('access')->defaultNull()->end()
+                ->scalarNode('name')->defaultNull()->end()
+            ->end()
+        ->end();
+
+        return $rootNode;
+    }
+
+    public function addOperationGetConfig()
+    {
+        $treeBuilder = new TreeBuilder(self::GET);
+        $rootNode    = $treeBuilder->getRootNode();
+
+        $rootNode = $this->addAccessConfig($rootNode);
+
+        return $rootNode;
+    }
+
+    public function addOperationListConfig()
+    {
+        $treeBuilder = new TreeBuilder(self::LIST);
+        $rootNode    = $treeBuilder->getRootNode();
+
+        $rootNode
+        ->children()
+            ->arrayNode('orderBy')
+                ->useAttributeAsKey('cc')
+                    ->prototype('scalar')->end()
+                ->end()
+            ->end()
+        ->end();
+
+        $rootNode = $this->addAccessConfig($rootNode);
+
+        return $rootNode;
+    }
+
+    public function addOperationCreateConfig()
+    {
+        $treeBuilder = new TreeBuilder(self::CREATE);
+        $rootNode    = $treeBuilder->getRootNode();
+
+        $rootNode = $this->addAccessConfig($rootNode);
+
+        return $rootNode;
+    }
+
+    public function addOperationUpdateConfig()
+    {
+        $treeBuilder = new TreeBuilder(self::UPDATE);
+        $rootNode    = $treeBuilder->getRootNode();
+
+        $rootNode = $this->addAccessConfig($rootNode);
+
+        return $rootNode;
+    }
+
+    public function addOperationDeleteConfig()
+    {
+        $treeBuilder = new TreeBuilder(self::DELETE);
+        $rootNode    = $treeBuilder->getRootNode();
+
+        $rootNode = $this->addAccessConfig($rootNode);
+
+        return $rootNode;
     }
 }
