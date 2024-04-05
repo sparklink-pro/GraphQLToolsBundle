23c2e46d7d068bcb3f84d78fafc90f13031fc175
Damien Belingheri <belingheridamien@gmail.com>
2024-04-05 14:41:00 +0200

    Refactor
    - Refactored the GraphQLToolsBundle class to extend from AbstractBundle instead of Bundle
    - Corrected the parameter name in services.yaml from 'graphql_tools_bundle_use_v2' to 'graph_ql_tools.use_v2' to match the parameter set in the loadExtension method of the GraphQLToolsBundle class

--- src/GraphQLToolsBundle.php
+++ src/GraphQLToolsBundle.php
@@ -5,15 +5,33 @@ declare(strict_types=1);
 namespace Sparklink\GraphQLToolsBundle;
 
 use Sparklink\GraphQLToolsBundle\DependencyInjection\CompilerPass\TypeManagerPass;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class GraphQLToolsBundle extends Bundle
+class GraphQLToolsBundle extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+            ->scalarNode('use_v2')->defaultFalse()->end()
+            ->end()
+        ;
+    }
+
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
 
         $container->addCompilerPass(new TypeManagerPass());
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    {
+        $containerBuilder->setParameter('graph_ql_tools.use_v2', $config['use_v2']);
+
+        $containerConfigurator->import('./Resources/config/services.yaml');
     }
 }
